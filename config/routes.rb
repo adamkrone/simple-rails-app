4a1d9ed1e8b469615b1f04d9e01cf79c70294c31
Adam Krone <krone.adam@gmail.com>
2014-06-10 23:32:57 -0500

    Adds simple post scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SimpleRailsApp::Application.routes.draw do
+  resources :posts
+
   root to: 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
